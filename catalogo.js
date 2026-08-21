1968dec169648a886cde5d2ac649ac9b3006a835
cubo830216-lgtm <cubo830216@gmail.com>
2026-08-21 09:31:38 -0600

    Fix 'Días' entry in catalogoGlobal
    Updated the 'Días' entry in the catalogoGlobal array.

--- catalogo.js
+++ catalogo.js
@@ -2,7 +2,7 @@ window.catalogoGlobal = [
   { "id": 1, "titulo": "Verbos 1", "grupo": "V1", "emoji": "🏃" },
   { "id": 2, "titulo": "Alimentos 1", "grupo": "A1", "emoji": "🍕" },
   { "id": 3, "titulo": "Objetos 1", "grupo": "O1", "emoji": "🛋️" },
-  { "id": 4, "titulo": "Días 1", "grupo": "D1", "emoji": "📅" },
+  { "id": 4, "titulo": "Días ", "grupo": "D1", "emoji": "📅" },
   { "id": 5, "titulo": "Números 1", "grupo": "N1", "emoji": "🔢" },
   { "id": 6, "titulo": "Colores 1", "grupo": "C1", "emoji": "🎨" },
   { "id": 7, "titulo": "Pronombres", "grupo": "P1", "emoji": "🗣️" },
