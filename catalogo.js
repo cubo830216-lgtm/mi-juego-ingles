79a1cd6de52eb538e5299e45cb526b5b9a8ddf41
cubo830216-lgtm <cubo830216@gmail.com>
2026-08-21 09:32:15 -0600

    Fix title for 'Números 1' in catalogoGlobal
    Updated the 'Números 1' title to 'Números'.

--- catalogo.js
+++ catalogo.js
@@ -3,7 +3,7 @@ window.catalogoGlobal = [
   { "id": 2, "titulo": "Alimentos 1", "grupo": "A1", "emoji": "🍕" },
   { "id": 3, "titulo": "Objetos 1", "grupo": "O1", "emoji": "🛋️" },
   { "id": 4, "titulo": "Días ", "grupo": "D1", "emoji": "📅" },
-  { "id": 5, "titulo": "Números 1", "grupo": "N1", "emoji": "🔢" },
+  { "id": 5, "titulo": "Números ", "grupo": "N1", "emoji": "🔢" },
   { "id": 6, "titulo": "Colores 1", "grupo": "C1", "emoji": "🎨" },
   { "id": 7, "titulo": "Pronombres", "grupo": "P1", "emoji": "🗣️" },
   { "id": 8, "titulo": "Meses", "grupo": "M1", "emoji": "🗓️" },
