c35ce0a8298b8713964adbbea8250f1cc0745e20
cubo830216-lgtm <cubo830216@gmail.com>
2026-08-21 10:00:10 -0600

    Modify catalogoGlobal entries and IDs
    Updated 'Verbos 2' entry ID from 1 to 11 and added new entry 'Colores 2' with ID 12.

--- catalogo.js
+++ catalogo.js
@@ -1,11 +1,12 @@
 window.catalogoGlobal = [
   { "id": 1, "titulo": "Verbos 1", "grupo": "V1", "emoji": "🏃" },
-  { "id": 1, "titulo": "Verbos 2", "grupo": "V2", "emoji": "🏃" },
+  { "id": 11, "titulo": "Verbos 2", "grupo": "V2", "emoji": "🏃" },
   { "id": 2, "titulo": "Alimentos 1", "grupo": "A1", "emoji": "🍕" },
   { "id": 3, "titulo": "Objetos 1", "grupo": "O1", "emoji": "🛋️" },
   { "id": 4, "titulo": "Días ", "grupo": "D1", "emoji": "📅" },
   { "id": 5, "titulo": "Números ", "grupo": "N1", "emoji": "🔢" },
   { "id": 6, "titulo": "Colores 1", "grupo": "C1", "emoji": "🎨" },
+  { "id": 12, "titulo": "Colores 2", "grupo": "C2", "emoji": "🎨" },
   { "id": 7, "titulo": "Pronombres", "grupo": "P1", "emoji": "🗣️" },
   { "id": 8, "titulo": "Meses", "grupo": "M1", "emoji": "🗓️" },
   { "id": 9, "titulo": "Preguntas", "grupo": "Q1", "emoji": "❓" },
