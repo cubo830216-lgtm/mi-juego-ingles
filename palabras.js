d3d65a168cc8a51962b3e388c77c20ea4642b81f
cubo830216-lgtm <cubo830216@gmail.com>
2026-08-21 07:03:40 -0600

    Add files via upload

---
+++ palabras.js
@@ -0,0 +1,776 @@
+window.palabrasGlobal = [
+  {
+    "id": 1,
+    "name": "apple",
+    "translation": "Manzana",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍎",
+    "urs_imagen": null
+  },
+  {
+    "id": 2,
+    "name": "pear",
+    "translation": "Pera",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍐",
+    "urs_imagen": null
+  },
+  {
+    "id": 3,
+    "name": "banana",
+    "translation": "Plátano",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍌",
+    "urs_imagen": null
+  },
+  {
+    "id": 4,
+    "name": "melon",
+    "translation": "Melón",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍈",
+    "urs_imagen": null
+  },
+  {
+    "id": 5,
+    "name": "watermelon",
+    "translation": "Sandía",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍉",
+    "urs_imagen": null
+  },
+  {
+    "id": 7,
+    "name": "orange",
+    "translation": "Naranja",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍊",
+    "urs_imagen": null
+  },
+  {
+    "id": 8,
+    "name": "strawberry",
+    "translation": "Fresa",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍓",
+    "urs_imagen": null
+  },
+  {
+    "id": 9,
+    "name": "kiwi",
+    "translation": "Kiwi",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥝",
+    "urs_imagen": null
+  },
+  {
+    "id": 10,
+    "name": "cherry",
+    "translation": "Cereza",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍒",
+    "urs_imagen": null
+  },
+  {
+    "id": 11,
+    "name": "bread",
+    "translation": "Pan",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍞",
+    "urs_imagen": null
+  },
+  {
+    "id": 12,
+    "name": "cheese",
+    "translation": "Queso",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🧀",
+    "urs_imagen": null
+  },
+  {
+    "id": 13,
+    "name": "milk",
+    "translation": "Leche",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥛",
+    "urs_imagen": null
+  },
+  {
+    "id": 14,
+    "name": "coffee",
+    "translation": "Café",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "☕",
+    "urs_imagen": null
+  },
+  {
+    "id": 15,
+    "name": "chocolate",
+    "translation": "Chocolate",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍫",
+    "urs_imagen": null
+  },
+  {
+    "id": 16,
+    "name": "chicken",
+    "translation": "Pollo",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍗",
+    "urs_imagen": null
+  },
+  {
+    "id": 17,
+    "name": "meat",
+    "translation": "Carne",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥩",
+    "urs_imagen": null
+  },
+  {
+    "id": 18,
+    "name": "fish",
+    "translation": "Pescado",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🐟",
+    "urs_imagen": null
+  },
+  {
+    "id": 19,
+    "name": "grapes",
+    "translation": "Uvas",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍇",
+    "urs_imagen": null
+  },
+  {
+    "id": 20,
+    "name": "pumpkin",
+    "translation": "Calabaza",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🎃",
+    "urs_imagen": null
+  },
+  {
+    "id": 21,
+    "name": "potato",
+    "translation": "Papa",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥔",
+    "urs_imagen": null
+  },
+  {
+    "id": 22,
+    "name": "cucumber",
+    "translation": "Pepino",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥒",
+    "urs_imagen": null
+  },
+  {
+    "id": 23,
+    "name": "tomato",
+    "translation": "Tomate",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍅",
+    "urs_imagen": null
+  },
+  {
+    "id": 24,
+    "name": "carrot",
+    "translation": "Zanahoria",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥕",
+    "urs_imagen": null
+  },
+  {
+    "id": 25,
+    "name": "egg",
+    "translation": "Huevo",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🥚",
+    "urs_imagen": null
+  },
+  {
+    "id": 26,
+    "name": "butter",
+    "translation": "Mantequilla",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🧈",
+    "urs_imagen": null
+  },
+  {
+    "id": 27,
+    "name": "salt",
+    "translation": "Sal",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🧂",
+    "urs_imagen": null
+  },
+  {
+    "id": 28,
+    "name": "sugar",
+    "translation": "Azúcar",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "🍬",
+    "urs_imagen": null
+  },
+  {
+    "id": 31,
+    "name": "water",
+    "translation": "Agua",
+    "grupo": "A1",
+    "color": "#000000",
+    "emoji": "💧",
+    "urs_imagen": null
+  },
+  {
+    "id": 32,
+    "name": "run",
+    "translation": "Correr",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🏃",
+    "urs_imagen": null
+  },
+  {
+    "id": 33,
+    "name": "jump",
+    "translation": "Saltar",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🦘",
+    "urs_imagen": null
+  },
+  {
+    "id": 34,
+    "name": "eat",
+    "translation": "Comer",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🍕",
+    "urs_imagen": null
+  },
+  {
+    "id": 35,
+    "name": "drink",
+    "translation": "Beber",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🥤",
+    "urs_imagen": null
+  },
+  {
+    "id": 36,
+    "name": "sleep",
+    "translation": "Dormir",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "😴",
+    "urs_imagen": null
+  },
+  {
+    "id": 37,
+    "name": "read",
+    "translation": "Leer",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "📚",
+    "urs_imagen": null
+  },
+  {
+    "id": 38,
+    "name": "write",
+    "translation": "Escribir",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "✍️",
+    "urs_imagen": null
+  },
+  {
+    "id": 39,
+    "name": "dance",
+    "translation": "Bailar",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "💃",
+    "urs_imagen": null
+  },
+  {
+    "id": 40,
+    "name": "sing",
+    "translation": "Cantar",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🎤",
+    "urs_imagen": null
+  },
+  {
+    "id": 41,
+    "name": "walk",
+    "translation": "Caminar",
+    "grupo": "V1",
+    "color": "#000000",
+    "emoji": "🚶",
+    "urs_imagen": null
+  },
+  {
+    "id": 42,
+    "name": "monday",
+    "translation": "Lunes",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "🌙",
+    "urs_imagen": null
+  },
+  {
+    "id": 43,
+    "name": "tuesday",
+    "translation": "Martes",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "🔥",
+    "urs_imagen": null
+  },
+  {
+    "id": 44,
+    "name": "wednesday",
+    "translation": "Miércoles",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "🌎",
+    "urs_imagen": null
+  },
+  {
+    "id": 45,
+    "name": "thursday",
+    "translation": "Jueves",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "⚡",
+    "urs_imagen": null
+  },
+  {
+    "id": 46,
+    "name": "friday",
+    "translation": "Viernes",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "🎉",
+    "urs_imagen": null
+  },
+  {
+    "id": 47,
+    "name": "saturday",
+    "translation": "Sábado",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "☀️",
+    "urs_imagen": null
+  },
+  {
+    "id": 48,
+    "name": "sunday",
+    "translation": "Domingo",
+    "grupo": "D1",
+    "color": "#000000",
+    "emoji": "🌞",
+    "urs_imagen": null
+  },
+  {
+    "id": 49,
+    "name": "one",
+    "translation": "Uno",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "1️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 50,
+    "name": "two",
+    "translation": "Dos",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "2️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 51,
+    "name": "three",
+    "translation": "Tres",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "3️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 52,
+    "name": "four",
+    "translation": "Cuatro",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "4️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 53,
+    "name": "five",
+    "translation": "Cinco",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "5️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 54,
+    "name": "six",
+    "translation": "Seis",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "6️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 55,
+    "name": "seven",
+    "translation": "Siete",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "7️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 56,
+    "name": "eight",
+    "translation": "Ocho",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "8️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 57,
+    "name": "nine",
+    "translation": "Nueve",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "9️⃣",
+    "urs_imagen": null
+  },
+  {
+    "id": 58,
+    "name": "ten",
+    "translation": "Diez",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔟",
+    "urs_imagen": null
+  },
+  {
+    "id": 59,
+    "name": "eleven",
+    "translation": "Once",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 60,
+    "name": "twelve",
+    "translation": "Doce",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 61,
+    "name": "thirteen",
+    "translation": "Trece",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 62,
+    "name": "fourteen",
+    "translation": "Catorce",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 63,
+    "name": "fifteen",
+    "translation": "Quince",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 64,
+    "name": "sixteen",
+    "translation": "Dieciséis",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 65,
+    "name": "seventeen",
+    "translation": "Diecisiete",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 66,
+    "name": "eighteen",
+    "translation": "Dieciocho",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 67,
+    "name": "nineteen",
+    "translation": "Diecinueve",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 68,
+    "name": "twenty",
+    "translation": "Veinte",
+    "grupo": "N1",
+    "color": "#000000",
+    "emoji": "🔢",
+    "urs_imagen": null
+  },
+  {
+    "id": 69,
+    "name": "fork",
+    "translation": "Tenedor",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🍴",
+    "urs_imagen": null
+  },
+  {
+    "id": 70,
+    "name": "spoon",
+    "translation": "Cuchara",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🥄",
+    "urs_imagen": null
+  },
+  {
+    "id": 71,
+    "name": "chair",
+    "translation": "Silla",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🪑",
+    "urs_imagen": null
+  },
+  {
+    "id": 72,
+    "name": "table",
+    "translation": "Mesa",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🛋️",
+    "urs_imagen": null
+  },
+  {
+    "id": 73,
+    "name": "sofa",
+    "translation": "Sofá",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🛋️",
+    "urs_imagen": null
+  },
+  {
+    "id": 74,
+    "name": "door",
+    "translation": "Puerta",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🚪",
+    "urs_imagen": null
+  },
+  {
+    "id": 75,
+    "name": "fan",
+    "translation": "Ventilador",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🌀",
+    "urs_imagen": null
+  },
+  {
+    "id": 76,
+    "name": "television",
+    "translation": "Televisor",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "📺",
+    "urs_imagen": null
+  },
+  {
+    "id": 77,
+    "name": "radio",
+    "translation": "Radio",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "📻",
+    "urs_imagen": null
+  },
+  {
+    "id": 78,
+    "name": "plate",
+    "translation": "Plato",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🍽️",
+    "urs_imagen": null
+  },
+  {
+    "id": 79,
+    "name": "napkin",
+    "translation": "Servilleta",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🧻",
+    "urs_imagen": null
+  },
+  {
+    "id": 80,
+    "name": "oven",
+    "translation": "Horno",
+    "grupo": "O1",
+    "color": "#000000",
+    "emoji": "🧱",
+    "urs_imagen": null
+  },
+  {
+    "id": 81,
+    "name": "pink",
+    "translation": "Rosa",
+    "grupo": "C1",
+    "color": "#ff69b4",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 82,
+    "name": "red",
+    "translation": "Rojo",
+    "grupo": "C1",
+    "color": "#f44336",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 83,
+    "name": "blue",
+    "translation": "Azul",
+    "grupo": "C1",
+    "color": "#2196f3",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 84,
+    "name": "green",
+    "translation": "Verde",
+    "grupo": "C1",
+    "color": "#4caf50",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 85,
+    "name": "yellow",
+    "translation": "Amarillo",
+    "grupo": "C1",
+    "color": "#ffeb3b",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 86,
+    "name": "orange",
+    "translation": "Naranja",
+    "grupo": "C1",
+    "color": "#ff9800",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 87,
+    "name": "purple",
+    "translation": "Morado",
+    "grupo": "C1",
+    "color": "#9c27b0",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 88,
+    "name": "black",
+    "translation": "Negro",
+    "grupo": "C1",
+    "color": "#212121",
+    "emoji": null,
+    "urs_imagen": null
+  },
+  {
+    "id": 89,
+    "name": "white",
+    "translation": "Blanco",
+    "grupo": "C1",
+    "color": "#ffffff",
+    "emoji": null,
+    "urs_imagen": null
+  }
+];
