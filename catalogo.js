35eb82c3b4bd1573062e9f164593767b49b5e74b
cubo830216-lgtm <cubo830216@gmail.com>
2026-08-25 22:40:18 -0600

    Refactor catalogoGlobal structure and content

--- catalogo.js
+++ catalogo.js
@@ -3,26 +3,30 @@ window.catalogoGlobal = [
   { id: 19, titulo: "Verbos", grupo: "XV", emoji: "🏃" },
   { id: 20, titulo: "Colores", grupo: "XC", emoji: "🎨" },
   { id: 21, titulo: "Gramatica", grupo: "XG", emoji: "📝" },
+  { id: 22, titulo: "Ser Humano", grupo: "XH", emoji: "📝" },
   // --- Hijjos
   { "id": 1, "titulo": "Verbos 1", "grupo": "V1", "emoji": "🏃" },
   { "id": 11, "titulo": "Verbos 2", "grupo": "V2", "emoji": "🏃" },
   { "id": 6, "titulo": "Colores 1", "grupo": "C1", "emoji": "🎨" },
   { "id": 12, "titulo": "Colores 2", "grupo": "C2", "emoji": "🎨" },
   { "id": 12, "titulo": "Colores 3", "grupo": "C3", "emoji": "🎨" },
+  { "id": 10, "titulo": "Auxiliares", "grupo": "G1", "emoji": "⚡" },
+  { "id": 7, "titulo": "Pronombres", "grupo": "G2", "emoji": "🗣️" },
+    { "id": 16, "titulo": "Cuerpo Humano", "grupo": "H1", "emoji": "🧍" },
+  { "id": 17, "titulo": "Emociones", "grupo": "H2", "emoji": "😊" },
+  { "id": 18, "titulo": "Sensaciones", "grupo": "H3", "emoji": "🤒" },
   // --- Directos
   { "id": 2, "titulo": "Alimentos 1", "grupo": "A1", "emoji": "🍕" },
   { "id": 3, "titulo": "Objetos 1", "grupo": "O1", "emoji": "🛋️" },
   { "id": 4, "titulo": "Días ", "grupo": "D1", "emoji": "📅" },
   { "id": 5, "titulo": "Números ", "grupo": "N1", "emoji": "🔢" },
-  { "id": 7, "titulo": "Pronombres", "grupo": "G2", "emoji": "🗣️" },
+  
   { "id": 8, "titulo": "Meses", "grupo": "M1", "emoji": "🗓️" },
   { "id": 9, "titulo": "Preguntas", "grupo": "Q1", "emoji": "❓" },
-  { "id": 10, "titulo": "Auxiliares", "grupo": "G1", "emoji": "⚡" },
+  
   { "id": 13, "titulo": "Transporte", "grupo": "T1", "emoji": "🚗" },
   { "id": 14, "titulo": "Escuela", "grupo": "E1", "emoji": "🏫" },
-  { "id": 15, "titulo": "Ropa", "grupo": "R1", "emoji": "👕" },
-  { "id": 16, "titulo": "Cuerpo Humano", "grupo": "H1", "emoji": "🧍" },
-  { "id": 17, "titulo": "Emociones", "grupo": "H2", "emoji": "😊" },
-  { "id": 18, "titulo": "Sensaciones", "grupo": "H3", "emoji": "🤒" },
-  { id: 22, titulo: "Ser Humano", grupo: "XH", emoji: "📝" },
+  { "id": 15, "titulo": "Ropa", "grupo": "R1", "emoji": "👕" }
+
+  
 ];
